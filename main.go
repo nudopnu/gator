81d0e6c73a140a617571bf91db5694c42859b275
nudopnu <forsuse@live.de>
2024-10-14 17:39:45 +0200

    Added cli option to login

--- main.go
+++ main.go
@@ -1,22 +1,74 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/nudopnu/gator/internal/config"
 )
 
+type (
+	state struct {
+		cfg *config.Config
+	}
+	command struct {
+		name string
+		args []string
+	}
+	commands struct {
+		handler map[string]func(*state, command) error
+	}
+)
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handler[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.handler[cmd.name]
+	if !ok {
+		return fmt.Errorf("handler for command '%s' not found", cmd.name)
+	}
+	return handler(s, cmd)
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("expecting name as an argument")
+	}
+	username := cmd.args[0]
+	err := s.cfg.SetUser(username)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("set username to '%s'\n", username)
+	return nil
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 		return
 	}
-	cfg.SetUser("nudopnu")
-	cfg, err = config.Read()
+	s := state{
+		cfg: &cfg,
+	}
+	cmds := commands{
+		handler: map[string]func(*state, command) error{},
+	}
+	cmds.register("login", handlerLogin)
+	if len(os.Args) < 2 {
+		log.Fatal("no command provided")
+	}
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
+	}
+	err = cmds.run(&s, cmd)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	fmt.Printf("%+v\n", cfg)
 }
