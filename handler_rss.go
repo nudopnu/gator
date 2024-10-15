196c92489e7432b3cd8ae9756168539eb68c5f50
nudopnu <forsuse@live.de>
2024-10-15 10:46:45 +0200

    Added addfeed command

--- handler_rss.go
+++ handler_rss.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"time"
 
+	"github.com/google/uuid"
+	"github.com/nudopnu/gator/internal/database"
 	"github.com/nudopnu/gator/internal/rss"
 )
 
@@ -18,5 +22,24 @@ func handlerAgg(s *state, cmd command) error {
 		return fmt.Errorf("error fetching feed: %w", err)
 	}
 	fmt.Printf("%+v\n", feed)
+	return nil
+}
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.args) < 2 {
+		return errors.New("please provide a name and a url")
+	}
+	name := cmd.args[0]
+	feedURL := cmd.args[1]
+	currentUser := s.cfg.CurrentUserName
+	user, err := s.db.GetUser(context.Background(), currentUser)
+	if err != nil {
+		return fmt.Errorf("invalid current user '%s': %w", currentUser, err)
+	}
+	feed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{ID: uuid.New(), CreatedAt: time.Now(), UpdatedAt: time.Now(), Name: name, Url: feedURL, UserID: user.ID})
+	if err != nil {
+		return fmt.Errorf("error creating feed: %w", err)
+	}
+	fmt.Printf("Successfully added feed:\n%+v\n", feed)
 	return nil
 }
